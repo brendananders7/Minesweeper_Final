1d9adfdf8ad7337c429fc5bfdf958788f15b5f43
Ivan-RC <46502817+Ivan-RC@users.noreply.github.com>
2020-01-26 19:41:30 -0700

    Upload updated ddl script

---
+++ dbo.Users.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName] NVARCHAR (50) NOT NULL,
+    [LastName]  NVARCHAR (50) NOT NULL,
+    [Gender]    NVARCHAR (50) NOT NULL,
+    [Age]       INT NOT NULL,
+    [State]     NVARCHAR (50) NOT NULL,
+    [Email]     NVARCHAR (50) NOT NULL,
+    [Username]  NVARCHAR (50) NOT NULL,
+    [Password]  NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
